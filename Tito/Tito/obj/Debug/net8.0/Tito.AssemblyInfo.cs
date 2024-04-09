1c9f999fd7821a8694a9ca7642a6ff9fbf536c06
EmilioBor <emilio3bolivar3@gmail.com>
2024-04-08 23:37:48 -0300

    dale

--- Tito/Tito/obj/Debug/net8.0/Tito.AssemblyInfo.cs
+++ Tito/Tito/obj/Debug/net8.0/Tito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ff297b002f5e272082c18bc234b7de27d7a2da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7440126e282da1e75aad9a47451017bb0695c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
