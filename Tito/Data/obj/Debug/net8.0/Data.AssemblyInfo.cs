a4ff297b002f5e272082c18bc234b7de27d7a2da
EmilioBor <emilio3bolivar3@gmail.com>
2024-04-08 23:27:32 -0300

    note

--- Tito/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Tito/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010c6ff43ff62bbcc38734f98b99714b7f235ce0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215bef9887a93c96e43dc1b31700f7bcae25ba4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
