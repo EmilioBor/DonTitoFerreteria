766f449f1b34e83eb1b6116061f9e7b8cfc2cc0d
EmilioBor <emilio3bolivar3@gmail.com>
2024-04-21 20:17:43 -0300

    cambios

--- Tito/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Tito/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae14d7eabc94cb277008fdefb6aa79eb1b9f1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d148c53bee463fa9a7f0403eb7ed83db61aedb32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
